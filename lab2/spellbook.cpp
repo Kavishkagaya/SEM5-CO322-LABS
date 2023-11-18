aa8705f9a1e063ec0eb136eb11ab7b125f9705ff
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-11-18 17:01:44 +0530

    removing unnessery sort

---
+++ lab2/spellbook.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int p, q;
+    cin >> p >> q;
+
+    int l[p], m[q];
+
+    for (int i = 0; i < p; i++)
+    {
+        cin >> l[i];
+    }
+
+    for (int i = 0; i < q; i++)
+    {
+        cin >> m[i];
+    }
+
+    for (int i = 0; i < p; i++)
+    {
+        for (int i = 0; i < q; i++)
+        {
+        }
+    }
+
+    return 0;
+}
